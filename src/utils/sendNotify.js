db1e932a9e9b709c9d21227e6802bb22554f09dd
ZackYoung <zackyoungh@163.com>
2025-09-12 09:36:27 +0800

    更新青龙通知接口

--- src/utils/sendNotify.js
+++ src/utils/sendNotify.js
@@ -372,24 +372,27 @@ async function sendNotify(text, desp, params = {}, author = '') {
     ]);
     // 由于上述两种微信通知需点击进去才能查看到详情，故text(标题内容)携带了账号序号以及昵称信息，方便不点击也可知道是哪个京东哪个活动
     text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
-    await Promise.all([
-        BarkNotify(text, desp, params), // iOS Bark APP
-        tgBotNotify(text, desp), // telegram 机器人
-        ddBotNotify(text, desp), // 钉钉机器人
-        qywxBotNotify(text, desp), // 企业微信机器人
-        qywxamNotify(text, desp), // 企业微信应用消息推送
-        iGotNotify(text, desp, params), // iGot
-        gobotNotify(text, desp), // go-cqhttp
-        gotifyNotify(text, desp), // gotify
-        ChatNotify(text, desp), // synolog chat
-        PushDeerNotify(text, desp), // PushDeer
-        aibotkNotify(text, desp), // 智能微秘书
-        fsBotNotify(text, desp), // 飞书机器人
-        smtpNotify(text, desp), // SMTP 邮件
-        pushMeNotify(text, desp, params), // PushMe
-        chronocatNotify(text, desp), // Chronocat
-        webhookNotify(text, desp), // 自定义通知
-    ]);
+
+    await QLAPI.systemNotify({title: text, content: desp})
+
+    // await Promise.all([
+    //     BarkNotify(text, desp, params), // iOS Bark APP
+    //     tgBotNotify(text, desp), // telegram 机器人
+    //     ddBotNotify(text, desp), // 钉钉机器人
+    //     qywxBotNotify(text, desp), // 企业微信机器人
+    //     qywxamNotify(text, desp), // 企业微信应用消息推送
+    //     iGotNotify(text, desp, params), // iGot
+    //     gobotNotify(text, desp), // go-cqhttp
+    //     gotifyNotify(text, desp), // gotify
+    //     ChatNotify(text, desp), // synolog chat
+    //     PushDeerNotify(text, desp), // PushDeer
+    //     aibotkNotify(text, desp), // 智能微秘书
+    //     fsBotNotify(text, desp), // 飞书机器人
+    //     smtpNotify(text, desp), // SMTP 邮件
+    //     pushMeNotify(text, desp, params), // PushMe
+    //     chronocatNotify(text, desp), // Chronocat
+    //     webhookNotify(text, desp), // 自定义通知
+    // ]);
 }
 
 function gotifyNotify(text, desp) {
